48fbb9f29d96b34e2dcee87b1571a3e74e9c542c
Dulasimca <dulasimca@gmail.com>
2020-12-15 20:24:08 +0530

    login and dashboard ticket count controller code commit

--- Controllers/LoginController.cs
+++ Controllers/LoginController.cs
@@ -12,24 +12,25 @@ namespace WebApplication1.Controllers
 {
     [Route("api/[controller]")]
     [ApiController]
-    public class DashboardTicketCountController : ControllerBase
+    public class LoginController : ControllerBase
     {
         [HttpGet("{id}")]
-        public string Get(string userId)
+        public string Get(string username)
         {
             ManageSQLConnection sqlConnection = new ManageSQLConnection();
             DataSet ds = new DataSet();
             try
             {
                 List<KeyValuePair<string, string>> sqlParameters = new List<KeyValuePair<string, string>>();
-                sqlParameters.Add(new KeyValuePair<string, string>("@userId", userId));
-                ds = sqlConnection.GetDataSetValues("GetTicketsCount", sqlParameters);
-                return JsonConvert.SerializeObject(ds.Tables);
+                sqlParameters.Add(new KeyValuePair<string, string>("@username", username));
+                ds = sqlConnection.GetDataSetValues("GetLoginDetails", sqlParameters);
+                return JsonConvert.SerializeObject(ds.Tables[0]);
             }
             finally
             {
                 ds.Dispose();
             }
         }
+
     }
 }
